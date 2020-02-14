ae58d11feb1374b73107163e7a9af52ac25a1f45
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-14 01:59:30 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,11 +4,12 @@ def hello(array)
   i = 0
   while i < array.length
   collection << yield(array[i])
+  
   binding.pry
     yield(array[i])
     i += 1
   end
 end
 
-binding.pry
+
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
